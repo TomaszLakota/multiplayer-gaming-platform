d5c1b710831d92e2df36cbeeacbdaad2ff697a4f
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2019-06-11 23:49:17 +0200

    now kicks user to main page, if logged in

--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -1,21 +1,26 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
 import SignUpForm from "./SignUpForm";
+import Authorized from "./AuthHOC";
+import { Redirect } from "react-router-dom";
 
 class RegistrationPage extends Component {
-  state = {};
-  render() {
-    return (
-      <React.Fragment>
-        <Navbar />
-        <div className="row justify-content-center">
-          <div className="col-4">
-            <SignUpForm />
-          </div>
-        </div>
-      </React.Fragment>
-    );
-  }
+   state = {};
+   render() {
+      if (this.props.loggedIn && this.props.loaded) {
+         return <Redirect to="/" push />;
+      }
+      return (
+         <React.Fragment>
+            <Navbar />
+            <div className="row justify-content-center">
+               <div className="col-4">
+                  <SignUpForm />
+               </div>
+            </div>
+         </React.Fragment>
+      );
+   }
 }
 
-export default RegistrationPage;
+export default Authorized(RegistrationPage);
